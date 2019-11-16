712257e642563cce290917ccb069f8345bd9e520
no <weathercontrol@tuta.io>
2019-11-15 19:56:58 -0800

    Renames client-lib so it can actually be imported

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ from clientlib import Message
 
 msg = Message('anonymous', 'example', 0, 0, 'some message here', 'localhost', 9999)
 msg.assemble()
-print(msg)
+print(msg + '\n')
 
 msg = Message('anonymous', 'example', 2, 1, 'some message here', 'localhost', 9999)
-msg.assemble()
+repr(msg.assemble())
 print(msg)
