5c0febaf7517a6b00b4ddb249cc0ed88efe7f95f
no <weathercontrol@tuta.io>
2019-11-15 19:57:39 -0800

    Undoes previous

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ from clientlib import Message
 
 msg = Message('anonymous', 'example', 0, 0, 'some message here', 'localhost', 9999)
 msg.assemble()
-print(msg + '\n')
+print(msg)
 
 msg = Message('anonymous', 'example', 2, 1, 'some message here', 'localhost', 9999)
-repr(msg.assemble())
+msg.assemble()
 print(msg)
